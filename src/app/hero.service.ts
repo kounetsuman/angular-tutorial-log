35e11a102883d1a05e765308a26e1eebd04b7d0c
kounetsuman <punkrou404@gmail.com>
2021-04-07 17:28:51 +0900

    TUTORIAL: https://angular.jp/tutorial/toh-pt5
    ---
    
    さまざまなコンポーネント間を行き来するためにAngularルーターを追加しました。
    AppComponentを、<a>リンクと<router-outlet>をもつナビゲーション・シェルに変更しました。
    AppRoutingModuleでルーターを設定しました。
    ルート、リダイレクトするルート、およびパラメータ付きルートを定義しました。
    アンカー要素でrouterLinkディレクティブを使用しました。
    密接に結合されたmaster/detailビューをルート化した詳細ビューにリファクタリングしました。
    ユーザーが選択したヒーローの詳細ビューに移動するために、ルーターリンクのパラメータを使用しました。
    複数のコンポーネント間でHeroServiceを共有しました。

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,19 +1,31 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+const defaultHero: Hero = {
+  id: 0,
+  name: ''
+}
+
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
+    // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    const hero = HEROES.find(hero => hero.id === id);
+    return of(hero ? hero : defaultHero);
   }
 }
