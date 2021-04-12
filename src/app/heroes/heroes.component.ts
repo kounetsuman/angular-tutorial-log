b5e0b2c134fd9db8a780dc1ead67063e32867d02
kounetsuman <punkrou404@gmail.com>
2021-04-12 12:03:53 +0900

    TUTORIAL: https://angular.jp/tutorial/toh-pt6
    ---
    
    アプリにHTTPで必要な依存パッケージを追加しました。
    HeroServiceをリファクタリングして、Web APIからヒーローを読み込めるようにしました。
    HeroServiceを拡張して、post(), put(), そして delete()メソッドを使えるようにしました。
    コンポーネントを更新して、ヒーローを追加、更新、そして削除できるようにしました。
    インメモリWeb APIを設定しました。
    Observableをどのように扱うかを学びました。

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,10 +6,10 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
@@ -22,8 +22,18 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
   delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
   }
+
 }
